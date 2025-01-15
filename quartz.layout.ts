27a76942476a0865962cefec51f59e15a014a000
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2025-01-15 23:30:39 +0200

    Custom footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,17 +1,15 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import * as AvivsComponents from "./quartz/components/avivr"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
-  }),
+
+  // Use custom footer
+  footer: AvivsComponents.Footer({}),
 }
 
 // components for pages that display a single page (e.g. a single note)
---
+++ quartz/components/avivr/Footer.tsx
@@ -0,0 +1,46 @@
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
+import style from "../styles/footer.scss"
+// import { version } from "../../../package.json"
+// import { i18n } from "../../i18n"
+
+interface Options {
+  // links: Record<string, string>
+}
+
+export default ((opts?: Options) => {
+  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+    const year = new Date().getFullYear()
+    const iconsize = "2x" // lg, 2x, 3x, .., 10x
+    // const links = opts?.links ?? []
+    return (
+      <footer class={`${displayClass ?? ""}`}>
+        <p>Aviv A. Rosenberg © {year}.</p>
+        <ul>
+          <li>
+            <a href="mailto:aviv@avivr.net">
+              <i class={`fa-solid fa-envelope fa-${iconsize}`}></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://linkedin.com/in/avivrosenberg">
+              <i class={`fa-brands fa-linkedin fa-${iconsize}`}></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/avivrosenberg">
+              <i class={`fa-brands fa-github fa-${iconsize}`}></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://scholar.google.com/citations?user=L6h2cnsAAAAJ">
+              <i class={`ai ai-google-scholar-square ai-${iconsize}`}></i>
+            </a>
+          </li>
+        </ul>
+      </footer>
+    )
+  }
+
+  Footer.css = style
+  return Footer
+}) satisfies QuartzComponentConstructor
