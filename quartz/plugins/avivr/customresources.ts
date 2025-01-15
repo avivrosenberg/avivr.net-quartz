21e67c92261342ef4f4434bf1caded24895ca627
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2025-01-15 23:48:43 +0200

    Remove comment and imports

--- quartz/plugins/avivr/customresources.ts
+++ quartz/plugins/avivr/customresources.ts
@@ -1,6 +1,3 @@
-import rehypeCitation from "rehype-citation"
-import { PluggableList } from "unified"
-import { visit } from "unist-util-visit"
 import { QuartzTransformerPlugin } from "../types"
 import { JSResource, CSSResource } from "../../util/resources"
 
@@ -19,7 +16,6 @@ export const AddCustomResources: QuartzTransformerPlugin<Partial<Options>> = (us
       const css: CSSResource[] = []
 
       // Add fontawesome and academicons support.
-      // TODO: This is not the right place, but I'm not sure where else to put it.
       css.push({
         content: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css",
         inline: false,
